1f2a91362405a68241c5c17d919b96325ee67204
czivich <cpzivich@yahoo.com>
2018-04-27 22:37:42 -0400

    Added Crystal and Repair Alerts
    Added popup display showing a unit was healed or repaired

---
+++ Assets/Scripts/UIAlertPanel.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIAlertPanel : MonoBehaviour {
+
+	//variable for the managers
+	private GameManager gameManager;
+	private UIManager uiManager;
+
+	//variable to hold the alert counter
+	private float alertTimer;
+	private float alertTimerLimit = 3.0f;
+
+	//Use this for initialization
+	public void Init(){
+
+		//get the manager
+		gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+		uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+
+	}
+
+	// Use this for initialization
+	private void OnEnable () {
+
+		//set the border color
+		SetBorderColor();
+
+		//set the alert text
+		SetAlertText();
+
+		//reset the timer
+		alertTimer = 0.0f;
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+
+		//update the timer
+		alertTimer += Time.deltaTime;
+
+		//check if the timer has exceeded the limit
+		if (alertTimer >= alertTimerLimit) {
+
+			//close the panel
+			this.gameObject.SetActive(false);
+
+		}
+		
+	}
+
+	//this function sets the border color
+	private void SetBorderColor(){
+
+		//set the border color to match the current turn player
+		switch (gameManager.currentTurn) {
+
+		case Player.Color.Green:
+
+			this.GetComponent<Image> ().color = GameManager.greenColor;
+
+			break;
+
+		case Player.Color.Purple:
+
+			this.GetComponent<Image> ().color = GameManager.purpleColor;
+
+			break;
+
+		case Player.Color.Red:
+
+			this.GetComponent<Image> ().color = GameManager.redColor;
+
+			break;
+
+		case Player.Color.Blue:
+
+			this.GetComponent<Image> ().color = GameManager.blueColor;
+
+			break;
+
+		default:
+
+			break;
+
+		}
+
+	}
+
+	//this function sets the alert text
+	private void SetAlertText(){
+
+		//set the text
+		this.GetComponentInChildren<TextMeshProUGUI>().text = uiManager.uiAlertPanelText;
+
+	}
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,6 +17,39 @@ public class UIManager : MonoBehaviour {
 	//variable for the default main camera screen width
 	public readonly float defaultMainCameraWidth = .75f;
 
+	//object to hold the panel
+	public GameObject uiAlertPanel;
+
+	//variable for alert text string
+	public string uiAlertPanelText {get; private set;}
+
+	//unityActions
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealShipPhasorSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealShipTorpedoSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealShipStorageSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealShipCrewSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealShipEngineSectionAction;
+
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBasePhasorSection1Action;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBasePhasorSection2Action;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBaseTorpedoSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBaseCrewSectionAction;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBaseStorageSection1Action;
+	private UnityAction<CombatUnit,CombatUnit,CombatManager.CrystalType,int> crystalHealBaseStorageSection2Action;
+
+	private UnityAction<CombatUnit,CombatUnit> repairCrewShipPhasorSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewShipTorpedoSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewShipStorageSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewShipCrewSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewShipEngineSectionAction;
+
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBasePhasorSection1Action;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBasePhasorSection2Action;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBaseTorpedoSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBaseCrewSectionAction;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBaseStorageSection1Action;
+	private UnityAction<CombatUnit,CombatUnit> repairCrewBaseStorageSection2Action;
+
 	//this list is all of the action modes that we want to lock out the main action buttons/toggles in
 	public static List<GameManager.ActionMode> lockMenuActionModes = new List<GameManager.ActionMode>() {
 
@@ -40,6 +73,217 @@ public class UIManager : MonoBehaviour {
 
 		//autosize the text
 		//AutoSizeAllText ();
+
+		//make the alert panel inactive
+		uiAlertPanel.SetActive(false);
+
+		//initialize the alert panel
+		uiAlertPanel.GetComponent<UIAlertPanel>().Init();
+
+		//set the unity actions
+		SetUnityActions();
+
+		//add event listeners
+		AddEventListeners();
+
+	}
+
+	//this function sets the unityActions
+	private void SetUnityActions(){
+
+		crystalHealShipPhasorSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Phasor Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealShipTorpedoSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Torpedo Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealShipStorageSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Storage Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealShipCrewSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Crew Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealShipEngineSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Engine Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+
+
+		crystalHealBasePhasorSection1Action = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Phasor Section 1!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealBasePhasorSection2Action = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Phasor Section 2!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealBaseTorpedoSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Torpedo Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealBaseCrewSectionAction = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Crew Section!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealBaseStorageSection1Action = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Storage Section 1!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		crystalHealBaseStorageSection2Action = (selectedUnit, targetedUnit, crystalType, shieldsHealed) => {
+			SetAlertString (shieldsHealed + " Shields Restored to Storage Section 2!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+
+		repairCrewShipPhasorSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Phasor Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewShipTorpedoSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Torpedo Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewShipStorageSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Storage Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewShipCrewSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Crew Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewShipEngineSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Engine Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+
+
+		repairCrewBasePhasorSection1Action = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Phasor Section 1 Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewBasePhasorSection2Action = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Phasor Section 2 Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewBaseTorpedoSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Torpedo Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewBaseCrewSectionAction = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Crew Section Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewBaseStorageSection1Action = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Storage Section 1 Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+		repairCrewBaseStorageSection2Action = (selectedUnit, targetedUnit) => {
+			SetAlertString ("Storage Section 2 Repaired!");
+			uiAlertPanel.SetActive(false);
+			uiAlertPanel.SetActive(true);
+
+		};
+
+	}
+
+	//this function adds event listeners
+	private void AddEventListeners(){
+
+		//add listeners for using crystals to heal ships
+		CombatManager.OnCrystalUsedOnShipPhasorSection.AddListener(crystalHealShipPhasorSectionAction);
+		CombatManager.OnCrystalUsedOnShipTorpedoSection.AddListener(crystalHealShipTorpedoSectionAction);
+		CombatManager.OnCrystalUsedOnShipStorageSection.AddListener(crystalHealShipStorageSectionAction);
+		CombatManager.OnCrystalUsedOnShipCrewSection.AddListener(crystalHealShipCrewSectionAction);
+		CombatManager.OnCrystalUsedOnShipEngineSection.AddListener(crystalHealShipEngineSectionAction);
+
+		//add listeners for using crystals to heal bases
+		CombatManager.OnCrystalUsedOnBasePhasorSection1.AddListener(crystalHealBasePhasorSection1Action);
+		CombatManager.OnCrystalUsedOnBasePhasorSection2.AddListener(crystalHealBasePhasorSection2Action);
+		CombatManager.OnCrystalUsedOnBaseTorpedoSection.AddListener(crystalHealBaseTorpedoSectionAction);
+		CombatManager.OnCrystalUsedOnBaseCrewSection.AddListener(crystalHealBaseCrewSectionAction);
+		CombatManager.OnCrystalUsedOnBaseStorageSection1.AddListener(crystalHealBaseStorageSection1Action);
+		CombatManager.OnCrystalUsedOnBaseStorageSection2.AddListener(crystalHealBaseStorageSection2Action);
+
+		//add listeners for using repair crews on ships
+		CombatManager.OnRepairCrewUsedOnShipPhasorSection.AddListener(repairCrewShipPhasorSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipTorpedoSection.AddListener(repairCrewShipTorpedoSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipStorageSection.AddListener(repairCrewShipStorageSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipCrewSection.AddListener(repairCrewShipCrewSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipEngineSection.AddListener(repairCrewShipEngineSectionAction);
+
+		//add listeners for using repair crews on bases
+		CombatManager.OnRepairCrewUsedOnBasePhasorSection1.AddListener(repairCrewBasePhasorSection1Action);
+		CombatManager.OnRepairCrewUsedOnBasePhasorSection2.AddListener(repairCrewBasePhasorSection2Action);
+		CombatManager.OnRepairCrewUsedOnBaseTorpedoSection.AddListener(repairCrewBaseTorpedoSectionAction);
+		CombatManager.OnRepairCrewUsedOnBaseCrewSection.AddListener(repairCrewBaseCrewSectionAction);
+		CombatManager.OnRepairCrewUsedOnBaseStorageSection1.AddListener(repairCrewBaseStorageSection1Action);
+		CombatManager.OnRepairCrewUsedOnBaseStorageSection2.AddListener(repairCrewBaseStorageSection2Action);
 
 	}
 		
@@ -109,5 +353,53 @@ public class UIManager : MonoBehaviour {
 		textElement.fontSize = textSize;
 
 	}
-						
+
+	//this function sets the alert string
+	private void SetAlertString(string newString){
+
+		uiAlertPanelText = newString;
+
+	}
+
+	//this function handles onDestroy
+	private void OnDestroy(){
+
+		RemoveEventListeners ();
+
+	}
+
+	//this function removes event listeners
+	private void RemoveEventListeners(){
+
+		//remove listeners for using crystals to heal ships
+		CombatManager.OnCrystalUsedOnShipPhasorSection.RemoveListener(crystalHealShipPhasorSectionAction);
+		CombatManager.OnCrystalUsedOnShipTorpedoSection.RemoveListener(crystalHealShipTorpedoSectionAction);
+		CombatManager.OnCrystalUsedOnShipStorageSection.RemoveListener(crystalHealShipStorageSectionAction);
+		CombatManager.OnCrystalUsedOnShipCrewSection.RemoveListener(crystalHealShipCrewSectionAction);
+		CombatManager.OnCrystalUsedOnShipEngineSection.RemoveListener(crystalHealShipEngineSectionAction);
+
+		//remove listeners for using crystals to heal bases
+		CombatManager.OnCrystalUsedOnBasePhasorSection1.RemoveListener(crystalHealBasePhasorSection1Action);
+		CombatManager.OnCrystalUsedOnBasePhasorSection2.RemoveListener(crystalHealBasePhasorSection2Action);
+		CombatManager.OnCrystalUsedOnBaseTorpedoSection.RemoveListener(crystalHealBaseTorpedoSectionAction);
+		CombatManager.OnCrystalUsedOnBaseCrewSection.RemoveListener(crystalHealBaseCrewSectionAction);
+		CombatManager.OnCrystalUsedOnBaseStorageSection1.RemoveListener(crystalHealBaseStorageSection1Action);
+		CombatManager.OnCrystalUsedOnBaseStorageSection2.RemoveListener(crystalHealBaseStorageSection2Action);
+
+		//remove listeners for using repair crews on ships
+		CombatManager.OnRepairCrewUsedOnShipPhasorSection.RemoveListener(repairCrewShipPhasorSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipTorpedoSection.RemoveListener(repairCrewShipTorpedoSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipStorageSection.RemoveListener(repairCrewShipStorageSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipCrewSection.RemoveListener(repairCrewShipCrewSectionAction);
+		CombatManager.OnRepairCrewUsedOnShipEngineSection.RemoveListener(repairCrewShipEngineSectionAction);
+
+		//remove listeners for using repair crews on bases
+		CombatManager.OnRepairCrewUsedOnBasePhasorSection1.RemoveListener(repairCrewBasePhasorSection1Action);
+		CombatManager.OnRepairCrewUsedOnBasePhasorSection2.RemoveListener(repairCrewBasePhasorSection2Action);
+		CombatManager.OnRepairCrewUsedOnBaseTorpedoSection.RemoveListener(repairCrewBaseTorpedoSectionAction);
+		CombatManager.OnRepairCrewUsedOnBaseCrewSection.RemoveListener(repairCrewBaseCrewSectionAction);
+		CombatManager.OnRepairCrewUsedOnBaseStorageSection1.RemoveListener(repairCrewBaseStorageSection1Action);
+		CombatManager.OnRepairCrewUsedOnBaseStorageSection2.RemoveListener(repairCrewBaseStorageSection2Action);
+
+	}						
 }

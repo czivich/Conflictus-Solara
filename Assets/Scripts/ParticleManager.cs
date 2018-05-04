a6cac379b7c8d78a5d8aa6b5a8817b5d24ae9a92
czivich <cpzivich@yahoo.com>
2018-05-03 20:58:15 -0400

    Neutral Starbase Particles
    Added preliminary particles for neutral starbase lights.

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParticleManager : MonoBehaviour {
+
+	//variable to hold the parent gameObject
+	public GameObject particleEffectsCollector;
+
+	//variables to hold the particle effect object prefabs
+	public GameObject prefabSunParticleEffect;
+	public GameObject prefabNeutralStarbaseLightsEffect;
+
+	// Use this for initialization
+	public void Init () {
+		
+	}
+
+	//this function adds event listeners
+	private void AddEventListeners(){
+
+
+	}
+
+	//this function handles OnDestroy
+	private void OnDestroy(){
+
+		RemoveEventListeners ();
+
+	}
+
+	//this function removes event listeners
+	private void RemoveEventListeners(){
+
+
+
+	}
+
+}
